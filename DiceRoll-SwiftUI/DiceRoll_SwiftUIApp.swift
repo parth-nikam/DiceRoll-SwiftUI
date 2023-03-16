c33d67ef50f24086d15f67846ee6b542f4d252f4
parth-nikam <parthsnikam12co@gmail.com>
2023-03-16 21:39:41 +0530

    Initial Commit

---
+++ DiceRoll-SwiftUI/DiceRoll_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRoll_SwiftUIApp.swift
+//  DiceRoll-SwiftUI
+//
+//  Created by Parth Nikam on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRoll_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
